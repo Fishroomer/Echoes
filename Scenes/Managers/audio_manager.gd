c41fd8aca3d18329a174ef6880bc689cef374726
Fishroom <771522490@qq.com>
2026-04-11 20:38:11 +0800

    创建基础项目

---
+++ Scenes/Managers/audio_manager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@onready var sfx: Node = $SFX
+@onready var bgm_player: AudioStreamPlayer2D = $BGMPlayer
+
+enum Bus {Master,BGM,SFX}
+
+func play_sfx(sfxname:String):
+	var player := sfx.get_node(sfxname) as AudioStreamPlayer
+	if not player:
+		return
+	player.play()
+
+func play_bgm(stream:AudioStream):
+	if bgm_player.stream == stream and bgm_player.playing:
+		return
+	bgm_player.stream = stream
+	bgm_player.play()
+
+func get_volume(bus_index: int) -> float:
+	var db := AudioServer.get_bus_volume_db(bus_index)
+	return db_to_linear(db)
+	
+func set_volume(bus_index: int,v: float) -> void:
+	var db := linear_to_db(v)
+	AudioServer.set_bus_volume_db(bus_index,db)
+
+func setup_ui_sounds(node:Node) -> void:
+	var button := node as BaseButton
+	if button:
+		button.pressed.connect(play_sfx("buttonpress"))
+	for child in node.get_children():
+		setup_ui_sounds(child)
